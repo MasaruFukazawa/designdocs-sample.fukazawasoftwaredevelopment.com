bbe59e40af2937da9e120b2d3f7946c12c1b2aa9
Masaru Fukazawa <upw.ponsuke@gmail.com>
2025-03-25 00:24:20 +0900

    修正

--- source/conf.py
+++ source/conf.py
@@ -26,11 +26,11 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 #html_theme = 'alabaster'
 
 html_static_path = ['../_static']
 
 html_css_files = [
-    'custom.css',
+    #'custom.css',
 ]
